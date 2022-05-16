e2f6d2bcf51d9121e6341abb90911ceec3b6481d
jbeutner <32556523+jbeutner@users.noreply.github.com>
2022-05-16 16:55:19 -0500

    Update pgh_create_db.py

--- pgh_create_db.py
+++ pgh_create_db.py
@@ -4,16 +4,16 @@ import json
 import pandas as pd
 
 #Sold homes data
-sold_df = pd.read_excel('pgh_realestate_analysis/sold_df.xlsx')
-engine = create_engine('postgresql://postgres:*****@localhost:5432/pgh_realestate')
+sold_df = pd.read_excel('lcn_realestate_analysis/lincoln_sold_df.xlsx')
+engine = create_engine('postgresql://postgres:password@localhost:5432/lcn_db')
 sold_df.to_sql('sold', engine, if_exists='replace', method='multi', index=False)
 
 #Census data
-census_df = pd.read_excel('pgh_realestate_analysis/census_df.xlsx')
-engine = create_engine('postgresql://postgres:*****@localhost:5432/pgh_realestate')
+census_df = pd.read_excel('lcn_realestate_analysis/lincoln_census_df.xlsx')
+engine = create_engine('postgresql://postgres:password@localhost:5432/lcn_db')
 census_df.to_sql('census', engine, if_exists='replace', method='multi', index=False)
 
 #Tract crosswalk
-tract_crosswalk = pd.read_excel('pgh_realestate_analysis/tract_crosswalk.xlsx')
-engine = create_engine('postgresql://postgres:*****@localhost:5432/pgh_realestate')
+tract_crosswalk = pd.read_excel('lcn_realestate_analysis/lincoln_tract_crosswalk.xlsx')
+engine = create_engine('postgresql://postgres:password@localhost:5432/lcn_db')
 tract_crosswalk.to_sql('tract_crosswalk', engine, if_exists='replace', method='multi', index=False)
